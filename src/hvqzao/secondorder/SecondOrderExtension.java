0e6fd76ca8bdbdecca841b4db2e0c0b88ec7eba8
hvqzao <hvqzao@gmail.com>
2016-10-31 23:01:20 +0100

    Minor UI handling progress

--- src/hvqzao/secondorder/SecondOrderExtension.java
+++ src/hvqzao/secondorder/SecondOrderExtension.java
@@ -5,15 +5,20 @@ import burp.IBurpExtender;
 import burp.IBurpExtenderCallbacks;
 import burp.IContextMenuFactory;
 import burp.IContextMenuInvocation;
+import burp.IExtensionHelpers;
 import burp.IHttpListener;
 import burp.IHttpRequestResponse;
+import burp.IRequestInfo;
 import burp.ITab;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
@@ -27,21 +32,25 @@ import javax.swing.table.TableColumn;
 public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFactory, IHttpListener {
 
     private static IBurpExtenderCallbacks callbacks;
-    //private static IExtensionHelpers helpers;
+    private static IExtensionHelpers helpers;
     private JScrollPane optionsTab;
     //private JFrame burpFrame;
     private ImageIcon iconHelp;
     private SecondOrderOptions optionsPane;
-    private final ArrayList<Rule> rule = new ArrayList<>();
+    private final ArrayList<Rule> rules = new ArrayList<>();
     private RuleTableModel ruleTableModel;
     private final List<JMenuItem> contextMenu = new ArrayList<>();
+    private IContextMenuInvocation invocation;
+    private JMenu menu;
+    private JMenuItem addRequestMenuItem;
+    private Rule active;
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         // keep a reference to our callbacks object
         SecondOrderExtension.callbacks = callbacks;
         // obtain an extension helpers object
-        //helpers = callbacks.getHelpers();
+        helpers = callbacks.getHelpers();
         // set extension name
         callbacks.setExtensionName("Second Order");
         // draw UI
@@ -65,22 +74,25 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
             optionsDefaults.setEnabled(false);
             callbacks.customizeUiComponent(optionsDefaults);
             //
-            JTable tokenTable = optionsPane.getRuleTable();
+            JTable ruleTable = optionsPane.getRuleTable();
             // table
             ruleTableModel = new RuleTableModel();
-            //tokenTableSorter = new TableRowSorter<>(tokenTableModel);
-            tokenTable.setModel(ruleTableModel);
-            // optionsTokensTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-            // optionsTokensTable.getTableHeader().setReorderingAllowed(true);
-            tokenTable.setAutoCreateRowSorter(true);
-            //optionsTokensTable.setRowSorter(tokenTableSorter);
+            //ruleTableSorter = new TableRowSorter<>(ruleTableModel);
+            ruleTable.setModel(ruleTableModel);
+            // ruleTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            // ruleTable.getTableHeader().setReorderingAllowed(true);
+            ruleTable.setAutoCreateRowSorter(true);
+            //ruleTable.setRowSorter(ruleTableSorter);
             for (int i = 0; i < ruleTableModel.getColumnCount(); i++) {
-                TableColumn column = tokenTable.getColumnModel().getColumn(i);
+                TableColumn column = ruleTable.getColumnModel().getColumn(i);
                 column.setMinWidth(20);
                 column.setPreferredWidth(ruleTableModel.getPreferredWidth(i));
+                int max = ruleTableModel.getMaxWidth(i);
+                if (max != -1) {
+                    column.setMaxWidth(max);
+                }
             }
-            callbacks.customizeUiComponent(tokenTable);
-
+            callbacks.customizeUiComponent(ruleTable);
             // options tab wrapper
             optionsTab = new JScrollPane(optionsPane, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
             callbacks.customizeUiComponent(optionsTab);
@@ -88,26 +100,56 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
             //
             JSplitPane optionsRuleTableSplitPane = optionsPane.getRuleTableSplitPane();
             optionsRuleTableSplitPane.setDividerSize(10);
-            //optionsTokensTableSplitPane.setContinuousLayout(true);
+            //optionsRuleTableSplitPane.setContinuousLayout(true);
             optionsRuleTableSplitPane.setUI(new GlyphSplitPaneUI(optionsPane.getBackground())); // each need separate instance
             // add the custom tab to Burp's UI
             callbacks.addSuiteTab(SecondOrderExtension.this);
             // context menu
-            JMenu menu = new JMenu("Second Order");
-            JMenuItem addRequestMenuItem = new JMenuItem("Add request to extension");
+            menu = new JMenu("Second order");
+            addRequestMenuItem = new JMenuItem("Add request");
+            addRequestMenuItem.addActionListener((ActionEvent e) -> {
+                // add request
+                IHttpRequestResponse[] messages = invocation.getSelectedMessages();
+                if (messages.length == 1) {
+                    Rule rule = new Rule(messages[0]);
+                    int row = rules.size();
+                    rules.add(rule);
+                    ruleTableModel.fireTableRowsInserted(row, row);
+                }
+            });
             menu.add(addRequestMenuItem);
             contextMenu.add(menu);
             callbacks.registerContextMenuFactory(SecondOrderExtension.this);
+            // remove rule
+            optionsPane.getRemoveRule().addActionListener((ActionEvent e) -> {
+                int selected = ruleTable.getSelectedRow();
+                if (selected == - 1) {
+                    return;
+                }
+                int index = ruleTable.convertRowIndexToModel(ruleTable.getSelectedRow());
+                rules.remove(index);
+                int row = rules.size();
+                ruleTableModel.fireTableRowsDeleted(row, row);
+            });
+            // clear rules
+            optionsPane.getClearRules().addActionListener((ActionEvent e) -> {
+                if (rules.isEmpty()) {
+                    return;
+                }
+                rules.clear();
+                int row = rules.size();
+                ruleTableModel.fireTableRowsDeleted(row, row);
+            });
             // get burp frame and tabbed pane handler
             //burpFrame = (JFrame) SwingUtilities.getWindowAncestor(optionsTab);
             //
-            int row = rule.size();
-            rule.add(new Rule());
-            rule.add(new Rule());
-            rule.add(new Rule());
-            ruleTableModel.fireTableRowsInserted(row, row);            
+            //int row = rules.size();
+            //rules.add(new Rule());
+            //rules.add(new Rule());
+            //rules.add(new Rule());
+            //ruleTableModel.fireTableRowsInserted(row, row);
             //
-            //callbacks.printOutput("Loaded.");
+            callbacks.printOutput("Loaded.");
         });
     }
 
@@ -137,6 +179,10 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
     //
     @Override
     public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
+        IHttpRequestResponse[] messages = invocation.getSelectedMessages();
+        menu.setEnabled(messages != null);
+        addRequestMenuItem.setEnabled(messages != null && messages.length == 1);
+        this.invocation = invocation;
         return contextMenu;
     }
 
@@ -145,13 +191,35 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
     //
     class Rule {
 
+        private final IHttpRequestResponse requestResponse;
+        private final String method;
+        private final URL url;
+
+        public Rule(IHttpRequestResponse requestResponse) {
+            this.requestResponse = requestResponse;
+            IRequestInfo requestInfo = helpers.analyzeRequest(requestResponse);
+            method = requestInfo.getMethod();
+            url = requestInfo.getUrl();
+        }
+
+        public IHttpRequestResponse getRequestResponse() {
+            return requestResponse;
+        }
+
+        public String getMethod() {
+            return method;
+        }
+
+        public URL getUrl() {
+            return url;
+        }
     }
 
     class RuleTableModel extends AbstractTableModel {
 
         @Override
         public int getRowCount() {
-            return rule.size();
+            return rules.size();
         }
 
         @Override
@@ -159,11 +227,31 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
             switch (columnIndex) {
                 case 0:
                     return true;
+                case 3:
+                    return true;
                 default:
                     return false;
             }
         }
-        
+
+        @Override
+        public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+            Rule rule = rules.get(rowIndex);
+            // active
+            if (columnIndex == 0) {
+                if (active == rule) {
+                    active = null;
+                } else {
+                    active = rule;
+                }
+                for (int i = 0; i < rules.size(); i++) {
+                    fireTableCellUpdated(i, columnIndex);
+                }
+                JLabel state = optionsPane.getState();
+                state.setText(active != null ? "Active" : "<html><i style='color:#e58900'>Inactive</i></html>");
+            }
+        }
+
         @Override
         public String getColumnName(int column) {
             switch (column) {
@@ -189,7 +277,7 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
                     return String.class;
             }
         }
-        
+
         @Override
         public int getColumnCount() {
             return 4;
@@ -197,22 +285,29 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
 
         @Override
         public Object getValueAt(int rowIndex, int columnIndex) {
-             switch (columnIndex) {
+            Rule rule = rules.get(rowIndex);
+            switch (columnIndex) {
                 case 0:
-                    return false;
+                    return rule.equals(active);
+                case 1:
+                    return rule.getMethod();
+                case 2:
+                    return rule.getUrl();
+                case 3:
+                    return rule.getRequestResponse().getComment();
                 default:
                     return "";
-            }           
+            }
         }
 
         public int getPreferredWidth(int column) {
             switch (column) {
                 case 0:
-                    return 80;
+                    return 60;
                 case 1:
-                    return 80;
+                    return 60;
                 case 2:
-                    return 180;
+                    return 200;
                 case 3:
                     return 120;
                 default:
@@ -220,5 +315,19 @@ public class SecondOrderExtension implements IBurpExtender, ITab, IContextMenuFa
             }
         }
 
+        public int getMaxWidth(int column) {
+            switch (column) {
+                case 0:
+                    return 60;
+                case 1:
+                    return 60;
+                case 2:
+                    return -1;
+                case 3:
+                    return 120;
+                default:
+                    return -1;
+            }
+        }
     }
 }
--- src/hvqzao/secondorder/SecondOrderOptions.java
+++ src/hvqzao/secondorder/SecondOrderOptions.java
@@ -7,6 +7,7 @@ package hvqzao.secondorder;
 
 import burp.IBurpExtenderCallbacks;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 
@@ -46,6 +47,18 @@ public class SecondOrderOptions extends javax.swing.JPanel {
         return ruleTableSplitPane;
     }
 
+    public JButton getRemoveRule() {
+        return removeRule;
+    }
+
+    public JButton getClearRules() {
+        return clearRules;
+    }
+
+    public JLabel getState() {
+        return state;
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -66,6 +79,12 @@ public class SecondOrderOptions extends javax.swing.JPanel {
         jPanel3 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         ruleTable = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        scanner = new javax.swing.JCheckBox();
+        intruder = new javax.swing.JCheckBox();
+        extender = new javax.swing.JCheckBox();
+        state = new javax.swing.JLabel();
 
         setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
@@ -88,17 +107,17 @@ public class SecondOrderOptions extends javax.swing.JPanel {
 
         clearRules.setText("Clear");
 
-        ruleTableSplitPane.setDividerLocation(300);
+        ruleTableSplitPane.setDividerLocation(450);
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 59, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 114, Short.MAX_VALUE)
+            .addGap(0, 152, Short.MAX_VALUE)
         );
 
         ruleTableSplitPane.setRightComponent(jPanel3);
@@ -118,6 +137,21 @@ public class SecondOrderOptions extends javax.swing.JPanel {
 
         ruleTableSplitPane.setLeftComponent(jScrollPane1);
 
+        jLabel2.setText("Extension state:");
+
+        jLabel4.setText("Tools in scope:");
+
+        scanner.setSelected(true);
+        scanner.setText("Scanner");
+
+        intruder.setSelected(true);
+        intruder.setText("Intruder");
+
+        extender.setSelected(true);
+        extender.setText("Extender");
+
+        state.setText("<html><i style='color:#e58900'>Inactive</i></html>");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -126,38 +160,60 @@ public class SecondOrderOptions extends javax.swing.JPanel {
                 .addComponent(optionsHelp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(optionsRewriteTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(optionsDefaults, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 445, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(clearRules, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(removeRule, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(ruleTableSplitPane, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))))
+                        .addComponent(ruleTableSplitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(state, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(scanner)
+                        .addGap(6, 6, 6)
+                        .addComponent(intruder)
+                        .addGap(8, 8, 8)
+                        .addComponent(extender)
+                        .addContainerGap())))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(optionsHelp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(optionsDefaults, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(optionsRewriteTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(16, 16, 16)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(removeRule)
+                        .addComponent(optionsHelp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(clearRules)
-                        .addContainerGap(64, Short.MAX_VALUE))
-                    .addComponent(ruleTableSplitPane, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                        .addComponent(optionsDefaults, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(optionsRewriteTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(16, 16, 16)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(removeRule)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(clearRules))
+                            .addComponent(ruleTableSplitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(scanner)
+                    .addComponent(intruder)
+                    .addComponent(extender)
+                    .addComponent(jLabel2)
+                    .addComponent(state, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, 0))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -181,7 +237,11 @@ public class SecondOrderOptions extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton clearRules;
+    private javax.swing.JCheckBox extender;
+    private javax.swing.JCheckBox intruder;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
@@ -191,5 +251,7 @@ public class SecondOrderOptions extends javax.swing.JPanel {
     private javax.swing.JButton removeRule;
     private javax.swing.JTable ruleTable;
     private javax.swing.JSplitPane ruleTableSplitPane;
+    private javax.swing.JCheckBox scanner;
+    private javax.swing.JLabel state;
     // End of variables declaration//GEN-END:variables
 }
